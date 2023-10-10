1039c2d51bfe2c29ac73cd4580bfd7535652cc94
tharun647 <112852656+tharun647@users.noreply.github.com>
2023-10-10 08:40:22 +0530

    Add files via upload

---
+++ DecimaltoBinary.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class DecimaltoBinary
+{  
+public static void main(String args[])
+{  
+System.out.println(Integer.toBinaryString(10));  
+System.out.println(Integer.toBinaryString(21));  
+System.out.println(Integer.toBinaryString(31));  
+}
+}  
