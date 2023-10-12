978f49de12cef03ec4efc625183788d0b5cf5dab
tharun647 <112852656+tharun647@users.noreply.github.com>
2023-10-12 08:52:07 +0530

    Add files via upload

---
+++ mergelist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class mergelist{
+
+    public static void main(String[] args)
+    {
+ArrayList<Integer> l1 = new ArrayList<Integer>();
+        l1.add(1);
+        l1.add(3);
+        l1.add(5);
+ArrayList<Integer> l2 = new ArrayList<Integer>();
+        l2.add(2);
+        l2.add(4);
+        l2.add(6);
+ArrayList<Integer> merge = new ArrayList<Integer>();
+        merge.addAll(l1);
+        merge.addAll(l2);
+System.out.println("L1 : "+l1);
+System.out.println("L2 : "+l2);
+System.out.println("Merged : "+merge);
+}
+}
