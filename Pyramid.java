ecc1095b7bf8abfa1e20e9ff781d46e85ba72b51
tharun647 <112852656+tharun647@users.noreply.github.com>
2023-10-12 10:11:30 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Pyramid
+{
+public static void main(String[] args)
+{
+int i,j;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number to be printed:");int x=s.nextInt();
+System.out.println("max number of times to be printed");int n=s.nextInt();
+for(i=1;i<=n;i++)
+{
+for(j=1;j<i;j++)
+{
+System.out.print(x+"");
+}
+System.out.println();
+}
+for(i=0;i<=n;i++)
+{
+for(j=n;j>i;j--)
+{
+System.out.print(x+"");
+}
+System.out.println();
+}
+}
+}
